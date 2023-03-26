ee2fae1761b02b412730107ef3a90c33bae0a395
cepeprecious <cepeprecious00@gmail.com>
2023-03-26 19:34:11 +0800

    updated changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://caringstore.xyz/';
+// $config['base_url'] = 'https://caringstore.xyz/';
+$config['base_url'] = 'http://localhost/caring-store/';
 
 /*
 |--------------------------------------------------------------------------
